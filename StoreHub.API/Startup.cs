405a82dd707b3faf63ad758860ee4b1bc8423add
my-app <fuentesjoshua0924@gmail.com>
2026-02-28 21:54:23 +0800

    Swagger Setup commit

---
+++ StoreHub.API/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace StoreHub.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Swagger (Swashbuckle)
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            #region Dependency Injection
+            // Register application services and repositories
+
+            #endregion
+
+            #region Swagger Configuration
+
+            #endregion
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Store API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
